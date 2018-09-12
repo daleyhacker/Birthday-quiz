4596df9e1c081c900b232d1c2a86e539bfa20556
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-12 11:58:32 -0400

    Updated from Brython Server: 9/12/2018 11:58:32 AM

--- birthday.py
+++ birthday.py
@@ -31,3 +31,18 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+name = input("Hello, what is your name? ")
+month = input("Hi "+ name + "(, what was the name of the month you were born in? ")
+if month == 
+
+
+year = input("And what year were you born in, " + name + "? ")
+day = input("And the day? ")
+
+
+
+
+
+
+
+
