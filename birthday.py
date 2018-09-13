93c9ae028f82108f6541dbc5298fe0eab4520b8c
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-13 14:46:06 -0400

    Updated from Brython Server: 9/13/2018 2:46:05 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Patrick Daley
+Credit: I asked Mr. Dennison a few questions
 Assignment:
 
 Your program will ask the user the following questions, in this order:
